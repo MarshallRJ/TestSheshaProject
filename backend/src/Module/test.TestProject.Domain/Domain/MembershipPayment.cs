f8af87889f3a062eaafc6096bcfd8c06e7575215
Robert Marshall <Robert.Marshall@Boxfusion.io>
2024-07-09 16:45:26 +0200

    Add membership payment to Domain

---
+++ backend/src/Module/test.TestProject.Domain/Domain/MembershipPayment.cs
@@ -0,0 +1,30 @@
+﻿using Abp.Domain.Entities.Auditing;
+using Boxfusion.Membership.Common.Domain.Domain;
+using Shesha.Domain.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Boxfusion.Membership.Common.Domain.Domain
+{
+    /// <summary>
+    ///
+    /// </summary>
+    [Entity(TypeShortAlias = "TP.MembershipPayment")]
+    public class MembershipPayment : FullAuditedEntity<Guid>
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        public virtual Member Member { get; set; }
+        /// <summary>
+        /// The payment amount
+        /// </summary>
+        public virtual double Amount { get; set; }
+        /// <summary>
+        /// The date when the payment was made
+        /// </summary>
+        public virtual DateTime PaymentDate { get; set; }
+    }
+}
---
+++ backend/src/Module/test.TestProject.Domain/Migrations/M20240709150100.cs
@@ -0,0 +1,33 @@
+﻿using FluentMigrator;
+using Shesha.FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Boxfusion.Membership.Common.Domain.Migrations
+{
+    [Migration(20240709150100)]
+    public class M20240709150100 : Migration
+    {
+        /// <summary>
+        /// Code to execute when executing the migrations
+        /// </summary>
+        public override void Up()
+        {
+            Create.Table("TP_MembershipPayments")
+                .WithIdAsGuid()
+                .WithFullAuditColumns()
+                .WithForeignKeyColumn("MemberId", "Core_Persons").Nullable()
+                .WithColumn("Amount").AsDouble().Nullable()
+                .WithColumn("PaymentDate").AsDateTime().Nullable();
+        }
+        /// <summary>
+        /// Code to execute when rolling back the migration
+        /// </summary>
+        public override void Down()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
